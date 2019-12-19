b54227f4ee98aa65cfdcf66e38044d1430ad3044
seferovramin7 <seferovramin7@gmail.com>
2019-12-19 15:42:38 +0400

    test

--- src/main/java/app/OnlineShopController.java
+++ src/main/java/app/OnlineShopController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class OnlineShopController {
     @GetMapping("/")
     String hello(){
-        return "Hello World!";
+        return "Hello World !!";
     }
 }
